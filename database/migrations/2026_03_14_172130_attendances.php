0ed61d9517976c7f1bc9cdac997f52f455d59e3d
davina <davinavc18@gmail.com>
2026-03-15 00:31:13 +0700

    create attendances table

---
+++ database/migrations/2026_03_14_172130_attendances.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('latitude', 50)->nullable();
+            $table->string('longitude', 50)->nullable();
+            $table->string('address', 500)->nullable();
+            $table->enum('status', ['attend', 'sick', 'leave', 'permit', 'business_trip', 'remote_work']);
+            $table->string('description', 500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
